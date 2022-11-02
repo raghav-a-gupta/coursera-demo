180ac7485cabbebe7375292ebf9af311fcdc13d3
raghav-a-gupta <raghav.a.gupta@in.ibm.com>
2022-11-02 17:37:45 +0530

    Adding 3 numbers feature is incorporated

--- add-numbers.py
+++ add-numbers.py
@@ -1,6 +1,7 @@
 x = input("Type a number: ")
 y = input("Type another number: ")
+z = input("Type another number: ")
 
-sum = int(x) + int(y)
+sum = int(x) + int(y) + int(z)
 
 print("The sum is: ", sum)
